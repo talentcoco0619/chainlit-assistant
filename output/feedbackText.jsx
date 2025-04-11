7146ffcc69d51450652e164b5e0df7ebf9930819
talentcoco0619 <collinboltoncoco@gmail.com>
2025-04-11 12:16:16 -0500

    feedbacktext updated

--- output/feedbackText.jsx
+++ output/feedbackText.jsx
@@ -6,23 +6,32 @@ export default function feedbackText() {
   const [showCards, setShowCards] = useState({});
 
   const toggleShowCard = (id) => {
-    setShowCards(prev => ({ ...prev, [id]: !prev[id] }));
+    setShowCards((prev) => ({ ...prev, [id]: !prev[id] }));
   };
 
   return (
     <Card className="max-w-md">
-   <br></br>   <CardContent>
-<div>
-      <Button className="mt-2" variant="outline" onClick={() => toggleShowCard('action.showcard')}>Action.ShowCard</Button>
-      {showCards['action.showcard'] && (
-        <Card className="max-w-md mt-2">
-          <CardContent> <br></br>
-<p className="text-base font-bold  mb-2">balabbal</p>
-        <p className="text-sm font-normal text-wrap mb-2">balabala</p>
-          </CardContent>
-        </Card>
-      )}
-      </div>
+      <br></br>{" "}
+      <CardContent>
+        <div>
+          <Button
+            className="mt-2"
+            variant="outline"
+            onClick={() => toggleShowCard("action.showcard")}
+          >
+            Action.ShowCard
+          </Button>
+          {showCards["action.showcard"] && (
+            <Card className="max-w-md mt-2">
+              <CardContent>
+                {" "}
+                <br></br>
+                <p className="text-base font-bold  mb-2">balabbal</p>
+                <p className="text-sm font-normal text-wrap mb-2">balabala</p>
+              </CardContent>
+            </Card>
+          )}
+        </div>
       </CardContent>
     </Card>
   );
