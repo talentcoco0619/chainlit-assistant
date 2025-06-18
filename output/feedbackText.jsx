34b101ee197141e1a23cf3eb452d9a37c962ffbf
talentcoco0619 <collinboltoncoco@gmail.com>
2025-06-18 08:54:36 -0500

    feedback jsx updated

--- output/feedbackText.jsx
+++ output/feedbackText.jsx
@@ -1,30 +1,30 @@
-import { Card, CardContent } from '@/components/ui/card'
-import { useState } from 'react'
-import { Button } from '@/components/ui/button'
+import { Card, CardContent } from "@/components/ui/card";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
 
 export default function feedbackText() {
-  const [showCards, setShowCards] = useState({})
+  const [showCards, setShowCards] = useState({});
 
   const toggleShowCard = (id) => {
-    setShowCards((prev) => ({ ...prev, [id]: !prev[id] }))
-  }
+    setShowCards((prev) => ({ ...prev, [id]: !prev[id] }));
+  };
 
   return (
     <Card className="max-w-md">
-      <br></br>{' '}
+      <br></br>{" "}
       <CardContent>
         <div>
           <Button
             className="mt-2"
             variant="outline"
-            onClick={() => toggleShowCard('action.showcard')}
+            onClick={() => toggleShowCard("action.showcard")}
           >
             Action.ShowCard
           </Button>
-          {showCards['action.showcard'] && (
+          {showCards["action.showcard"] && (
             <Card className="max-w-md mt-2">
               <CardContent>
-                {' '}
+                {" "}
                 <br></br>
                 <p className="text-base font-bold  mb-2">Paragraph Content</p>
                 <p className="text-sm font-normal text-wrap mb-2">balabala</p>
@@ -34,5 +34,5 @@ export default function feedbackText() {
         </div>
       </CardContent>
     </Card>
-  )
+  );
 }
